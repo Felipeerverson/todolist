f49041ffd13c5f29ed733067031528881d412be7
Felipe Erverson <107085822+Felipeerverson@users.noreply.github.com>
2023-04-20 14:51:05 -0300

    Atualização e finalização do desenvolvimento.

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/models/todo.dart';
+import 'package:todolist/repositories/todo_repository.dart';
 import 'package:todolist/widgets/todo_list_item.dart';
 
 
@@ -12,8 +13,23 @@ class TodoListPage extends StatefulWidget {
 
 class _TodoListPageState extends State<TodoListPage> {
   final TextEditingController todoController = TextEditingController();
+  final TodoRepository todoRepository=TodoRepository();
 
   List<Todo> todos = [];
+
+  Todo? deletedTodo;
+  int? deletedTodoPos;
+
+  @override
+  void initState() {
+    super.initState();
+
+    todoRepository.getTodoList().then((value) {
+      setState(() {
+        todos = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +76,7 @@ class _TodoListPageState extends State<TodoListPage> {
               Icons.add,
               size: 30,
             ),
-          ),
+           ),
           ],
         ),
         SizedBox( height: 16,),
@@ -71,6 +87,7 @@ class _TodoListPageState extends State<TodoListPage> {
               for (Todo todo in todos)
                TodoListItem(
                  todo: todo,
+                 onDelete: onDelete,
                ),
         ],
        ),
@@ -86,20 +103,82 @@ class _TodoListPageState extends State<TodoListPage> {
       ),
       SizedBox (width: 8),
       ElevatedButton(
-      onPressed: ()  {},
+      onPressed:showDeleteTodosConfirmationDialog,
       style: ElevatedButton.styleFrom(
       primary: Color(0xff00d7f3),
       padding: EdgeInsets.all(14),
       ),
       child: Text('limpar tudo'),
-      ),
-      ],
-      ),
-      ],
-      ),
-      ),
-      ),
+              ),
+             ],
+            ),
+           ],
+          ),
+         ),
+        ),
       ),
     );
   }
-}
+  void onDelete(Todo todo){
+
+      deletedTodo = todo;
+      deletedTodoPos = todos.indexOf(todo);
+
+      setState(() {
+      todos.remove(todo);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Tarefa ${todo.title} foi removida com Sucesso',
+            style: TextStyle(color: Color(0xff060708)),
+          ),
+          backgroundColor: Colors.white,
+          action: SnackBarAction(
+            label: 'Desfazer',
+            textColor: const Color(0xff00d7f3),
+            onPressed: (){
+              setState(() {
+                todos.insert(deletedTodoPos!, deletedTodo!);
+              });
+            },
+          ),
+          duration: const Duration(seconds: 7),
+        ),
+     );
+   }
+  void showDeleteTodosConfirmationDialog() {
+    showDialog(
+      context: context,
+      builder: (context) =>
+          AlertDialog(
+            title: Text('Limpar Tudo'),
+            content: Text('Voce tem certeza que apagar todas as tarefas?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: TextButton.styleFrom(primary: Color(0xff00d7f3)),
+                child: Text('Cancelar'),
+              ),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  deleteAllTodos();
+                },
+                style: TextButton.styleFrom(primary: Colors.red),
+                child: Text('Limpar Tudo'),
+              ),
+            ],
+          ),
+       );
+     }
+     void deleteAllTodos(){
+      setState(() {
+        todos.clear();
+      });
+    }
+  }
--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,35 +1,58 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
+import 'package:todolist/models/todo.dart';
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key, required  this.title}) : super(key: key);
+  const TodoListItem({Key? key, required  this.todo, required this.onDelete,}) : super(key: key);
 
-  final String title;
+
+
+  final Todo todo;
+  final Function(Todo) onDelete;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.grey[200],
-      ),
-
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('20/11/2023',
-        style: TextStyle(
-          fontSize: 12,
-        ) ,
-      ),
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Slidable (
+        child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.grey[200],
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yy - HH:mm').format(todo.dateTime),
+              style: TextStyle(
+                fontSize: 12,
+              ),
             ),
-          ),
-        ],
+            Text(
+              todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+         ),
+        ),
+        endActionPane: ActionPane(
+          motion: const DrawerMotion(),
+          extentRatio: 0.20,
+          children: [
+           SlidableAction(
+
+             backgroundColor: Colors.red,
+             icon: Icons.delete,
+             label: 'Deletar',
+             onPressed: (BuildContext) => { onDelete(todo)},
+           ),
+         ],
+        ),
       ),
     );
   }
